1031dc6301569a2ee239d07ad8f2da0dd0b9899d
Linh Nguyen <tuo84526@temple.edu>
2022-02-04 23:44:14 -0500

    Create Greeting.java

---
+++ Labs/Lab 1/Greeting.java
@@ -0,0 +1,18 @@
+module greeting {
+}// Linh Nguyen
+// 1/10/2022
+// Assignment 1. Greetings
+//
+// Introduce my name, assignment's date, and print out greetings
+//
+
+package greeting;
+
+import java.io.*;
+
+public class Greeting {
+	public static void main(String[] str) {
+		System.out.println("Good morning, Mr. Liu.");
+		
+	}
+}
